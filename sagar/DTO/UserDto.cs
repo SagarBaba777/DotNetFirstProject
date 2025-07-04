371177f273f790d34aab008f4c6036394d322a4b
sagar <sagar.h@vmaxindia.com>
2025-07-04 17:59:07 +0530

    first commit

---
+++ sagar/DTO/UserDto.cs
@@ -0,0 +1,15 @@
+﻿namespace sagar.DTO
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; } = "User"; // Default role is User
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; } = null;
+        public bool IsActive { get; set; } = true; // Default is active
+
+    }
+}
